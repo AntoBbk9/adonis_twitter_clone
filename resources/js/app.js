1b4852c05d421310b1d8148778b8b612073ee93a
Antoinette Belebele <anthobelebele@gmail.com>
2024-07-26 12:34:52 +0100

    add javascript in application

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,22 +10,46 @@ console.log(buttonFollowing);
     element.classList.remove(className);
   }
   buttonExplore.addEventListener('click', ()=>{
-    if (buttonExplore.classList.contains('page-tab-active')) {
+    if(buttonExplore.classList.contains('page-tab-active')) {
         removeClass(buttonExplore, 'page-tab-active');
+        addClass(buttonFollowing, 'page-tab-active')
     }else{
         addClass(buttonExplore, 'page-tab-active');
-        removeClass(buttonFollowing, 'page-tab-active')
+        removeClass(buttonFollowing, 'page-tab-active');
     }
   });
 
   buttonFollowing.addEventListener('click', ()=>{
     if (buttonFollowing.classList.contains('page-tab-active')) {
         removeClass(buttonFollowing, 'page-tab-active');
+        removeClass(buttonExplore, 'page-tab-active')
+
     }else{
         addClass(buttonFollowing, 'page-tab-active');
-        removeClass(buttonExplore, 'page-tab-active')
+        removeClass(buttonExplore, 'page-tab-active');
     }
-  })
+  });
+
+  buttonExplore.addEventListener('click', ()=>{
+    if(buttonExplore.classList.contains('page-tab-disable')) {
+        removeClass(buttonExplore, 'page-tab-disable');
+        addClass(buttonFollowing, 'page-tab-disable')
+    }else{
+        addClass(buttonExplore, 'page-tab-disable');
+        removeClass(buttonFollowing, 'page-tab-disable');
+    }
+  });
+
+  buttonFollowing.addEventListener('click', ()=>{
+    if (buttonFollowing.classList.contains('page-tab-disable')) {
+        removeClass(buttonFollowing, 'page-tab-disable');
+        removeClass(buttonExplore, 'page-tab-disable')
+
+    }else{
+        addClass(buttonFollowing, 'page-tab-disable');
+        removeClass(buttonExplore, 'page-tab-disable');
+    }
+  });
 
   document.querySelector('.tweet-editor-form').addEventListener('submit', function(e) {
     e.preventDefault();
